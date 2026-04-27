8ff601cdc8fb3685469ee1e84a9d620477324863
maximivasiv <maximivasiv1@gmail.com>
2026-04-27 12:03:41 +0300

    Update main_task.py

--- main_task.py
+++ main_task.py
@@ -1,47 +1,10 @@
-from collections import UserDict
-
-
-class Field:
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-        return str(self.value)
-
-
-class Name(Field):
-    pass
-
-
-class Phone(Field):
-    def __init__(self, value):
-        if not (value.isdigit() and len(value) == 10):
-            raise ValueError("Phone must contain exactly 10 digits")
-        super().__init__(value)
-
-
 class Record:
     def __init__(self, name):
         self.name = Name(name)
         self.phones = []
 
     def add_phone(self, phone_number):
-        phone = Phone(phone_number)
-        self.phones.append(phone)
-
-    def remove_phone(self, phone_number):
-        for phone in self.phones:
-            if phone.value == phone_number:
-                self.phones.remove(phone)
-                return
-
-    def edit_phone(self, old_number, new_number):
-        for phone in self.phones:
-            if phone.value == old_number:
-                self.phones.remove(phone)
-                self.phones.append(Phone(new_number))
-                return
-        raise ValueError("Phone not found")
+        self.phones.append(Phone(phone_number))
 
     def find_phone(self, phone_number):
         for phone in self.phones:
@@ -49,25 +12,24 @@ class Record:
                 return phone
         return None
 
+    def remove_phone(self, phone_number):
+        phone = self.find_phone(phone_number)
+        if phone:
+            self.phones.remove(phone)
+        else:
+            raise ValueError("Phone not found")
+
+    def edit_phone(self, old_number, new_number):
+       
+        phone = self.find_phone(old_number)
+        if not phone:
+            raise ValueError("Phone not found")
+
+        new_phone = Phone(new_number)
+
+        self.remove_phone(old_number)
+        self.phones.append(new_phone)
+
     def __str__(self):
         phones = "; ".join(phone.value for phone in self.phones)
         return f"Contact name: {self.name.value}, phones: {phones}"
-
-
-class AddressBook(UserDict):
-
-    def add_record(self, record):
-        self.data[record.name.value] = record
-
-    def find(self, name):
-        return self.data.get(name)
-
-    def delete(self, name):
-        if name in self.data:
-            del self.data[name]
-
-    def __str__(self):
-        result = ""
-        for record in self.data.values():
-            result += str(record) + "\n"
-        return result.strip()
